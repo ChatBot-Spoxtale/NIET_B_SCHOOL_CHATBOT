198b7ab544fb4ec3344925a4c47718ac59c5a4ff
Sv-Namrata-Ag <sv.namrataagarwal@gmail.com>
2026-01-17 10:48:44 +0000

    adding program_key_features_scraper.py which is used to extract all the key features present on programs page

---
+++ scraper/pages/program_key_features_scraper.py
@@ -0,0 +1,83 @@
+import requests
+import hashlib
+import json
+import re
+import os
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+URL = "https://www.nietbschool.ac.in/program#features"
+OUTPUT_FILE = "../output/program_key_features.json"
+
+os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
+
+def normalize(text):
+    text = text.lower()
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
+
+def sha256_hash(text):
+    return hashlib.sha256(text.encode("utf-8")).hexdigest()
+
+def extract_key_features(soup):
+    section = soup.find("section", id="features")
+    if not section:
+        return []
+
+    features = []
+
+    cards = section.find_all("div", class_="bg-white")
+
+    for card in cards:
+        title_tag = card.find("h3")
+        desc_tag = card.find("p")
+
+        if not title_tag or not desc_tag:
+            continue
+
+        title = title_tag.get_text(strip=True)
+        description = desc_tag.get_text(strip=True)
+
+        if title and description:
+            features.append({
+                "title": title,
+                "description": description
+            })
+
+    return features
+
+#MAIN SCRAPER
+def scrape_key_features():
+    response = requests.get(URL, timeout=20)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    for tag in soup(["script", "style", "nav", "footer"]):
+        tag.decompose()
+
+    key_features = extract_key_features(soup)
+
+    combined_text = normalize(
+        " ".join(f["title"] + " " + f["description"] for f in key_features)
+    )
+
+    content_hash = sha256_hash(combined_text)
+
+    result = {
+        "page_type": "program_key_features",
+        "source_url": URL,
+        "last_scraped": datetime.utcnow().isoformat(),
+        "content_hash": content_hash,
+        "data": {
+            "key_features": key_features
+        }
+    }
+
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
+
+    print("Key Features scraped → program_key_features.json")
+
+if __name__ == "__main__":
+    scrape_key_features()

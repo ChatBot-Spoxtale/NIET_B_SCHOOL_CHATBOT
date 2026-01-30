a6203952aebe9511a4f56e74d70da193ed98b99f
svaadityakuamr8130-sys <sv.aadityakuamr8130@gmail.com>
2026-01-30 12:21:11 +0000

    updating app.py file for deployment purpose

--- RAG/app.py
+++ RAG/app.py
@@ -90,4 +90,4 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8060, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8060, reload=True)

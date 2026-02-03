d262cc686d768a6c72b08332e4f9c272046b283e
svaadityakuamr8130-sys <sv.aadityakuamr8130@gmail.com>
2026-02-03 12:28:24 +0530

    changing  main to app in app.py file

--- RAG/app.py
+++ RAG/app.py
@@ -107,5 +107,6 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8060, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8060, reload=True)
+
 
